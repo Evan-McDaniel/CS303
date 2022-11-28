b3927fdc6d2a046f77e78d6d70aa39ecea0c994e
Evan-McDaniel <emmcdaniel21@gmail.com>
2022-11-27 23:23:25 -0600

    Completed first question

---
+++ Assignment_3/Assignment_3.cpp
@@ -0,0 +1,30 @@
+// Assignment_3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Functions.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    //testing the function for question 1
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    cout << "Front of Q is: " << q.front();
+    move_to_rear(q);
+    cout << "\nFront of Q is: " << q.front();
+    //testing function for question 2
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ Assignment_3/Functions.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <queue>
+
+using namespace std;
+
+/**
+* 1
+* Function which takes the front of a queue and puts it at the end
+* @param: the queue to modify
+* @return: the same queue that was passed
+*/
+template <typename T>
+queue<T> move_to_rear(queue<T>& q)
+{
+	T front = q.front();
+	q.pop();
+	q.push(front);
+	return q;
+}
+
+/**
+* 2
+* Function to determine if a word is a palindrome or not
+* @param: the string to determine
+* @return: a boolean - true if is a palindrome
+*/
+bool isPalindrome1()
+{
+	return (1 == 1);
+}
+
+
